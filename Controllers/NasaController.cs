75c1f15aece6c4ff43280b52c63fceea7fdda1c2
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-09 16:02:13 -0500

    added url for NASA image of the day

--- Controllers/NasaController.cs
+++ Controllers/NasaController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AstroAPI.ViewModels;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
 
 namespace AstroAPI.Controllers
 {
@@ -11,12 +13,32 @@ namespace AstroAPI.Controllers
     [ApiController]
     public class NasaController : ControllerBase
     {
+
+        readonly IConfiguration _configuration;
+
+        private string NASA_API_KEY = "";
+        public NasaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            NASA_API_KEY = _configuration.GetValue<string>("NASA_API_KEY");
+
+        }
         [HttpGet("ping")]
         public ActionResult<BasicResponse> Ping()
         {
             return new BasicResponse { Message = "pong" };
         }
 
-        
+        [HttpGet("apod")]
+        public async Task<ActionResult<NasaResponse>> GetImageOfTheDay()
+        {
+            var client = new HttpClient();
+            var response = await client.GetAsync($"https://api.nasa.gov/planetary/apod?api_key={this.NASA_API_KEY}");
+            // TODO, cache things!
+            // response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadAsAsync<NasaResponse>();
+            return result;
+        }
+
     }
 }
---
+++ ViewModel/NasaResponse.cs
@@ -0,0 +1,16 @@
+namespace AstroAPI.ViewModels
+{
+    public class NasaResponse
+    {
+        public string Message { get; set; }
+        public string Copyright { get; set; }
+        public string Date { get; set; }
+        public string Explanation { get; set; }
+        public string HdUrl { get; set; }
+        public string media_type { get; set; }
+        public string Service_Version { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+
+}
