9b9f2777f1be8ce6693a4485b965d9ae15cf0fa8
Mark Dewey <mark.t.dewey@gmail.com>
2018-11-12 12:56:39 -0500

    added created date and expires times

--- Cache/CacheItem.cs
+++ Cache/CacheItem.cs
@@ -1,4 +1,4 @@
-
+using System;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
@@ -10,5 +10,7 @@ namespace AstroAPI.Cache
         public ObjectId Id { get; set; }
         public string Url { get; set; }
         public object Content { get; set; }
+        public DateTime CachedAt { get; set; } = DateTime.Now;
+        public DateTime expiresAt { get; set; } = DateTime.Now.AddDays(1);
     }
 }
